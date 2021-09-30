667c5b4bdd1111093c552ac9440c5a3bd6c27ac2
Amanda Shafack <shafack.likhene@gmail.com>
2021-09-30 22:28:25 +0100

    Format code using standard principles

--- revising_the_select_query_I/revising_the_select_query_I.sql
+++ revising_the_select_query_I/revising_the_select_query_I.sql
@@ -1,9 +1,9 @@
-/*
- * Query all columns for all American cities in the CITY table with populations larger than 100000.
- * The CountryCode for America is USA. The CITY table is described as follows:
- * Exercise Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
- */
- 
+-- Query all columns for all American cities in the CITY table with populations larger than 100000.
+-- The CountryCode for America is USA. The CITY table is described as follows:
+-- Exercise Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
 Select *
 From city
-where (population > 100000 and countrycode = 'USA');
+where (
+                population > 100000
+                and countrycode = 'USA'
+        );
