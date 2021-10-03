d61a88be913317278e0b02e10be3e9adb3f8ed61
Amanda Shafack <shafack.likhene@gmail.com>
2021-10-03 20:22:23 +0100

    Rename files to remove blank spaces

--- weather_observation_station/weather_observation _station_1.sql
+++
@@ -1,4 +0,0 @@
--- Query a list of CITY and STATE from the STATION table.
--- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
-select city, state
-from station;
--- weather_observation_station/weather_observation _station_3.sql
+++
@@ -1,7 +0,0 @@
--- Query a list of CITY names from STATION for cities that have an even ID number. 
--- Print the results in any order, but exclude duplicates from the answer.
--- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-3/problem
-select distinct city
-from station
-where id % 2 = 0
-order by city;
