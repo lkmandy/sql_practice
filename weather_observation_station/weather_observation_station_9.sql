7e465a4fcec516c94e2aa1639aae6e0cef04f1dd
Amanda Shafack <shafack.likhene@gmail.com>
2021-10-04 23:33:16 +0100

    List city names that don't begin with a vowel

---
+++ weather_observation_station/weather_observation_station_9.sql
@@ -0,0 +1,6 @@
+-- Query the list of CITY names from STATION that do not start with vowels. 
+-- Your result cannot contain duplicates.
+-- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+select distinct city
+from station
+where city not like '[aeiou]%';
