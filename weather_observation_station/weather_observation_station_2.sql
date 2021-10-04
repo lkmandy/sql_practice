8c5e6eb43742770fc7f61d7025653e842fd4d569
Amanda Shafack <shafack.likhene@gmail.com>
2021-10-04 23:31:31 +0100

    Print out the sum of all longitudes and latitudes of all cities in station

---
+++ weather_observation_station/weather_observation_station_2.sql
@@ -0,0 +1,12 @@
+-- Query the following two values from the STATION table:
+-- The sum of all values in LAT_N rounded to a scale of  decimal places.
+-- The sum of all values in LONG_W rounded to a scale of  decimal places.
+-- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-2/problem
+select cast(round(sum(lat_n), 2) as decimal(10, 2)) as "lat",
+        cast(round(sum(long_w), 2) as decimal(10, 2)) as "lon"
+from station;
+
+-- Alternative MySQL 
+-- select round(sum(lat_n), 2) as "lat",
+--         round(sum(long_w), 2) as "lon"
+-- from station;
