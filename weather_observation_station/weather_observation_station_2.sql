fbfaa32a24208556ec9893d8c7e6b1816a2cbc1d
Amanda Shafack <shafack.likhene@gmail.com>
2021-10-06 23:33:57 +0100

    Add a comment to depict the database type

--- weather_observation_station/weather_observation_station_2.sql
+++ weather_observation_station/weather_observation_station_2.sql
@@ -2,6 +2,7 @@
 -- The sum of all values in LAT_N rounded to a scale of  decimal places.
 -- The sum of all values in LONG_W rounded to a scale of  decimal places.
 -- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-2/problem
+-- MS SQL Server
 select cast(round(sum(lat_n), 2) as decimal(10, 2)) as "lat",
         cast(round(sum(long_w), 2) as decimal(10, 2)) as "lon"
 from station;
