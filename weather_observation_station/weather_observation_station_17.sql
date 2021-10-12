b5750ddbb2646ed606161f4cc9e21c532200e866
Amanda Shafack <shafack.likhene@gmail.com>
2021-10-12 23:39:28 +0100

    Fix merge conflict

--- weather_observation_station/weather_observation_station_17.sql
+++ weather_observation_station/weather_observation_station_17.sql
@@ -1,7 +1,7 @@
 -- Query the Western Longitude (LONG_W) where the smallest Northern Latitude (LAT_N) in STATION 
 -- is greater than 38.7780 . Round your answer to  decimal places.
 -- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-17/problem
-Select convert(decimal(20, 4), long_w)
+Select top 1 convert(decimal(20, 4), long_w)
 from station
-Group by long_w
-Having min(lat_n) > 38.7780;
+where lat_n > 38.7780
+order by lat_n;
