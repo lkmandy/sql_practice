35d107c9ae9e37979add7bd45cf16e95fcb1a7b5
Amanda Shafack <shafack.likhene@gmail.com>
2021-10-28 18:38:25 +0100

    Calculate the Manhattan distance between two points

--- weather_observation_station/weather_observation_station_18.sql
+++ weather_observation_station/weather_observation_station_18.sql
@@ -5,6 +5,8 @@
 -- d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
 -- Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
 -- Exercise Link: https://www.hackerrank.com/challenges/weather-observation-station-18/problem
-select
-from
-where;
+select convert(
+                decimal(20, 4),
+                (max(lat_n) - min(lat_n)) + (max(long_w) - min(long_w))
+        )
+from station;
